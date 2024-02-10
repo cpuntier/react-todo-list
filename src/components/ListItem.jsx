699aa44d683fa4cf100aab60424f4f6ccac2d37c
cpuntier <cpuntier@gmail.com>
2024-02-09 20:37:54 -0500

    some styling

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,11 +1,11 @@
-import { useRef, useState,useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 
 export default function ListItem(props) {
     console.log("RERENDER")
     const currentItem = props.data.find((item) => item.id === props.id);
     const inputRef = useRef(null);
-    console.log("This was found",props.data.find((item) => item.id === props.id))
-    console.log("current item",currentItem);
+    console.log("This was found", props.data.find((item) => item.id === props.id))
+    console.log("current item", currentItem);
 
     const [editListItem, setEditListItem] = useState(false);
     const [input, setInput] = useState("");
@@ -14,7 +14,7 @@ export default function ListItem(props) {
     function handleDelete() {
         const data = props.data
         const newData = data.filter((item) => item.id != props.id)
-   //     console.log(newData);
+        //     console.log(newData);
         props.setData(newData);
         setChecked(!checked);
     }
@@ -23,46 +23,50 @@ export default function ListItem(props) {
         setInput(inputRef.current.value);
     }
 
-    function savedListItem(){
-        const obj = {...props.data.find((item) => item.id == props.id)}
+    function savedListItem() {
+        const obj = { ...props.data.find((item) => item.id == props.id) }
         obj.content = input;
-        
-        props.data.splice(props.data.indexOf(props.data.find((item)=> props.id === item.id)),1,obj);
+
+        props.data.splice(props.data.indexOf(props.data.find((item) => props.id === item.id)), 1, obj);
         setEditListItem(false);
         props.setData(props.data);
     }
 
-    function checkHandler(){
+    function checkHandler() {
         currentItem.done = !currentItem.done;
         setChecked(!checked)
     }
 
-    function editHandler(){
+    function editHandler() {
         setInput(currentItem.content);
         setEditListItem(true);
     }
 
     return (
         <>
-            <li key = {props.id}>
+            <li key={props.id} style={{ display: "flex", justifyContent: "space-between" }}>
 
-                {editListItem ?
-                    <>
-                        <label >
-                            <input ref = {inputRef} type="text" value={input} onChange={editedListItem} />
-                        </label>
+                    {editListItem ?
+                        <>
+                            <label >
+                                <input ref={inputRef} type="text" value={input} onChange={editedListItem} />
+                            </label>
 
-                        <button onClick={savedListItem}>Save</button>
-                    </>
-                    :
-                    <>
-                        <label>
-                            <input type="checkbox" checked={currentItem.done}  onChange={checkHandler}/> <span style={{textDecoration: checked ? "line-through": ""}}>{currentItem.content}</span>
-                        </label>
+                            <button onClick={savedListItem}>Save</button>
+                        </>
+                        :
+                        <>
+                            <label>
+                                <input type="checkbox" checked={currentItem.done} onChange={checkHandler} /> <span style={{ textDecoration: checked ? "line-through" : "" }}>{currentItem.content}</span>
+                            </label>
+                            <div>
 
-                        <button onClick={() => {console.log(currentItem.content);setInput(currentItem.content);setEditListItem(true);}}>Edit</button>
-                        <button onClick={handleDelete} disabled = {!checked}>Delete</button>
-                    </>}
+                            <button onClick={() => { console.log(currentItem.content); setInput(currentItem.content); setEditListItem(true); }}>Edit</button>
+                            <button onClick={handleDelete} disabled={!checked}>Delete</button>
+                            </div>
+                        </>}
+
+
             </li>
         </>
     )
